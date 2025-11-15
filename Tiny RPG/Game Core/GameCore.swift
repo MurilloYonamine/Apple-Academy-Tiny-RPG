0ab5610eb3dc9262551af90a93d085ee64030a31
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-11-15 01:16:19 -0300

    Removido Método createOgre e Jogador balanceado

--- Tiny RPG/Game Core/GameCore.swift
+++ Tiny RPG/Game Core/GameCore.swift
@@ -14,63 +14,60 @@ class GameCore {
         let nome : String = readLine() ?? "";
         
         while !hasTheGameEnded {
-            let (character, equipment) = createPlayer(name: nome)
+            let (character, equipment) = createPlayer(name: nome);
             
             let player : Player = Player(
                 xp: 10,
                 characterData: character,
                 equipmentData: equipment
-            )
+            );
             let enemy : Enemy = generateEnemy();
             
+            print("")
             print("O que deseja fazer?")
             let options = [
                 "[1] Comecar Combate",
                 "[2] Sair do Jogo"
-            ]
+            ];
             
             let menu = Menu(options: options);
             let chosenOption = menu.getChosenOption();
             
-            handleOption(option: chosenOption, player: player, enemy: enemy)
+            handleOption(option: chosenOption, player: player, enemy: enemy);
         }
     }
     private func handleOption(option : Int, player : Player, enemy : Enemy) {
         switch option {
         case 1:
-            let combatSystem = CombatSystem(Player: player, Enemy: enemy)
-            combatSystem.startCombat();
+            print("")
+            let combatSystem = CombatSystem(Player: player, Enemies: [enemy])
+            combatSystem.startCombat()
+            print("")
         case 2:
             hasTheGameEnded = true;
         default:
-            print("Nao reconheco esse numero")
+            print("Nao reconheco esse numero");
         }
     }
+    
     private func generateEnemy() -> Enemy {
-        let (character, equipment) = createOgre(name: "Ogre")
-            
-        let enemy : Enemy = Enemy(
-            characterData: character,
-            equipmentData: equipment
-        )
-        
-        return enemy;
+        let enemyFactory = EnemyFactory()
+        return enemyFactory.createRandomEnemy()
     }
 
     // Deus eu queria fazer um json ou um txt, mas nao vai dar tempo
     private func createPlayer(name : String) -> (CharacterData, EquipmentData) {
         
-        let statsValue : Float = 5.0;
         let characterData : CharacterData = CharacterData(
             _name: name,
-            _level: Int(statsValue),
-            _life: 200,
-            _damage: 20,
-            _critical: statsValue,
-            _evasion: statsValue,
-            _resistance: statsValue,
-            _agility: statsValue
-        )
+            _level: 1,
+            _life: 100,
+            _damage: 10,
+            _critical: 5,
+            _evasion: 3,
+            _resistance: 4,
+            _agility: 4
+        );
         
         let requiredLevel : Int = 1;
         
@@ -78,80 +75,33 @@ class GameCore {
             _helmet: Helmet(
                 requiredLevel: requiredLevel,
                 name: "Capacete de Couro",
-                critical: statsValue
+                critical: 2
             ),
             
             _breastPlate: Breastplate(
                 requiredLevel: requiredLevel,
                 name: "Peitoral de Couro",
-                resistance: statsValue
+                resistance: 2
             ),
             
             _greaves: Greaves(
                 requiredLevel: requiredLevel,
                 name: "Grevas de Couro",
-                agility: statsValue
+                agility: 2
             ),
             
             _boot: Boot(
                 requiredLevel: requiredLevel,
                 name: "Bota de Couro",
-                evasion: statsValue
+                movementSpeed: 2
             ),
             
             _weapon: Weapon(
                 requiredLevel: requiredLevel,
                 name: "Espada de Madeira",
-                extraDamage: statsValue
+                extraDamage: 3
             )
-        )
-        
-        return (characterData, equipmentData);
-    }
-    private func createOgre(name : String) -> (CharacterData, EquipmentData) {
-        
-        let statsValue : Float = 3.0;
-        let characterData : CharacterData = CharacterData(
-            _name: name,
-            _level: Int(statsValue),
-            _life: 200,
-            _damage: statsValue,
-            _critical: statsValue,
-            _evasion: statsValue,
-            _resistance: statsValue,
-            _agility: statsValue
-        )
-        
-        let requiredLevel : Int = 1;
-        
-        let equipmentData : EquipmentData = EquipmentData(
-            _helmet: Helmet(
-                requiredLevel: requiredLevel,
-                name: "",
-                critical: statsValue
-            ),
-            
-            _breastPlate: Breastplate(
-                requiredLevel: requiredLevel,
-                name: "Peitoral de Couro"),
-            
-            _greaves: Greaves(
-                requiredLevel: requiredLevel,
-                name: ""
-            ),
-            
-            _boot: Boot(
-                requiredLevel: requiredLevel,
-                name: "",
-                evasion: statsValue
-            ),
-            
-            _weapon: Weapon(
-                requiredLevel: requiredLevel,
-                name: "Porrete",
-                extraDamage: statsValue
-            )
-        )
+        );
         
         return (characterData, equipmentData);
     }
