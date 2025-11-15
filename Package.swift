d7f7569dc5afe97b35799262af950e54a90df940
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-11-15 01:42:58 -0300

    Adicionei o Package.swift para o Tiny Rpg

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "TinyRPG",
+    targets: [
+        .executableTarget(
+            name: "TinyRPG",
+            path: "Tiny RPG"
+        )
+    ]
+)
