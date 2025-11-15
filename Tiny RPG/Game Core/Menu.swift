9e56dba7004953b3ba9ea581735fc2b1b3bb8e57
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-11-15 01:15:25 -0300

    Método showMenu simplificado a construção da mensagem e removido código desnecessário no inicializador

--- Tiny RPG/Game Core/Menu.swift
+++ Tiny RPG/Game Core/Menu.swift
@@ -8,20 +8,17 @@
 class Menu {
     private var _options : Array<String>;
     private var _chosenOption : Int = 0;
-    
+
     init(options: Array<String>, chosenOption: Int = 0) {
         self._options = options;
-        
-        showMenu(texts: _options)
+
+        showMenu(texts: _options);
         _chosenOption = Int(readLine() ?? "") ?? 0;
     }
-    
+
     public func showMenu(texts : Array<String>) {
-        var message : String = "";
-        for text in texts {
-            message = message + "\(text)\n"
-        }
-        print(message)
+        let message = texts.joined(separator: "\n");
+        print(message);
     }
     public func getChosenOption() -> Int {
         _chosenOption;
